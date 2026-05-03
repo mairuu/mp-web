0a162194ad06cf6dd582f699953f117e76c4ff95
mairuu <kmr.mairuu@gmail.com>
2026-05-03 17:23:19 +0700

    add: basic health check

---
+++ src/routes/health/+server.ts
@@ -0,0 +1,8 @@
+import { json, type RequestHandler } from "@sveltejs/kit";
+
+export const GET: RequestHandler = () => {
+    return json({
+        status: "ok",
+        timestamp: new Date().toISOString()
+    });
+}
